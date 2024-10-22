4ea69c7740062f0a035118219cd2907f69666c82
MykolaDekusar <ndekusar97@gmail.com>
2024-10-22 16:43:01 +0200

    First variable

--- Variabili/src/def/Main.java
+++ Variabili/src/def/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int x = 100; //initialization and declaration
+		System.out.println(x);
 	}
 
 }
